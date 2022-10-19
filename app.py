cec0bc4c32e1df3695e208b1e84e4c6474390055
byunboy2 <76236947+byunboy2@users.noreply.github.com>
2022-10-19 15:24:40 -0700

    Finished model and User Listing Template

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, redirect, render_template
+from models import db, connect_db, User
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blog_model'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
 db.create_all()
+
+@app.get("/")
+
+def load_home():
+    """Redirect to current list of users"""
+    return redirect("/users")
+
+@app.get("/users")
+def load_user():
+    """Load currently saved users"""
+    users = User.query.all()
+    return render_template("index.html", users = users)
--- models.py
+++ models.py
@@ -1,1 +1,18 @@
 """Models for Blogly."""
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+def connect_db(app):
+    """Connect to database."""
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model):
+    """User"""
+
+    __table__name = "User"
+
+    id = db.Column(db.Integer,primary_key=True,autoincrement=True)
+    first_name = db.Column(db.String(50),nullable = False, unique = True)
+    last_name = db.Column(db.String(50),nullable = False, unique = True)
+    image_url = db.Column(db.String(50),nullable = False, unique = True)
